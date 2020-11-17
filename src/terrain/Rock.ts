c29178c0d20866839b929a86d08ac7e395ca851d
Synnøve Sørensen <578010@stud.hvl.no>
2020-11-17 13:36:40 +0100

    Removed unused code

--- src/terrain/Rock.ts
+++ src/terrain/Rock.ts
@@ -1,29 +1,12 @@
-import { Mesh, MeshLambertMaterial, MeshStandardMaterial, Object3D, Scene, SphereGeometry, TextureLoader } from "three";
+import { Mesh, MeshStandardMaterial, Object3D, TextureLoader } from "three";
 import Entity from "src/entity";
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 
 export class Rock implements Entity {
     public object: Object3D;
-    public scene: Scene;
 
     constructor() {
-        // this.object = new Object3D();
-        // let rockGeometry = new SphereGeometry(0.7, 48, 48);
-        // let rockTexture = new TextureLoader().load('/resources/rock/rock_texture.jpg');
-        // let rockMaterial = new MeshStandardMaterial({
-        //     map: rockTexture,
-        //     color: 0xffffff,
-        //     roughness: 0.9
-        // });
-
-        // rockMaterial.bumpMap = new TextureLoader().load('/resources/rock/rock_normal.jpg');
-        // rockMaterial.bumpScale = 10;
-
-        // this.object = new Mesh(rockGeometry, rockMaterial);
-
         this.object = new Object3D();
-        this.scene = new Scene();
 
         let loader = new OBJLoader();
         loader.load('/resources/rock2/Rock_1.OBJ', (obj) => {
@@ -41,11 +24,9 @@ export class Rock implements Entity {
                 color: 0xffffff,
                 roughness: 0.9,
                 bumpMap: new TextureLoader().load('/resources/Rock2/rock/Rock_normal.jpg'),
-                bumpScale: 100
+                bumpScale: 1
             });
 
-            rockMaterial.bumpMap = new TextureLoader().load('/resources/Rock2/rock/Rock_normal.jpg');
-            rockMaterial.bumpScale = 1000;
             // OBJLoader returns a object with children
             obj.traverse(function (child) {
                 if (child instanceof Mesh) {
